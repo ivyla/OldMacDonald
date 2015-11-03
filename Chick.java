6fe60099f21da3421f9872945dec19fafc1af01d
Ivy La <ivyla3@gmail.com>
2015-11-02 22:46:34 -0800

    Changed

--- Chick.java
+++ Chick.java
@@ -1,4 +1,21 @@
 class Chick implements Animal 
 {     
+  private String myType;
+  private String mySound;
+  public Chick(String type, String sound, String sound2){
+  
+    myType = type;
+    if((int)(Math.random()*2 + 1) == 1)
+    mySound = sound2;
+    else
+    mySound = sound;
+  }
+  
+  public String getSound(){
+  return mySound;
+  }
+  public String getType(){
+  return myType;
+  }
      //your code here
 }
--- Farm.java
+++ Farm.java
@@ -1,4 +1,18 @@
-class Farm 
+public class Farm 
 {     
-   //your code here
+   private Animal[] aBunchOfAnimals = new Animal[3];
+  public Farm(){
+  aBunchOfAnimals[0] = new NamedCow("Cow", "Merrrrggh", "MOO");
+  aBunchOfAnimals[1] = new Chick("Chick", "Chirp", "Cluck");
+  aBunchOfAnimals[2] = new Pig("Pig", "Snort");
+  } 
+  
+  public void animalSounds(){
+  for(int nI = 0; nI < aBunchOfAnimals.length; nI++){
+  System.out.println(aBunchOfAnimals[nI].getType() + "goes" + aBunchOfAnimals[nI].getSound());
+  }
+  System.out.println("The cow is known as" + ((NamedCow)aBunchOfAnimals[0]).getName());
+  }
+ 
+  //your code here
 }
